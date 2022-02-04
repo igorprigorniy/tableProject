6e01a0aa6da73bf92b0664b52cc0c0d722bb84dc
igorprigorniy <igor.prigorniy@gmail.com>
2022-02-04 18:31:18 +0300

    Добавлен .gitignore, выровнены строки в main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import random
+import pandas as pd
+
 
 # Функция создания тестового датафрейма
 def createTestDataFrame(rows=1, names_male=[], names_female=[], surnames=[], **kwargs):
@@ -9,8 +10,8 @@ def createTestDataFrame(rows=1, names_male=[], names_female=[], surnames=[], **k
     for n in range(rows):
         data.append({})
 
-        gender = random.choice(['male', 'female'])
-        name = random.choice(names_male) if gender == 'male' else random.choice(names_female)
+        gender  = random.choice(['male', 'female'])
+        name    = random.choice(names_male)  if gender == 'male' else random.choice(names_female)
         surname = random.choice(surnames)[0] if gender == 'male' else random.choice(surnames)[1]
 
         data[n]['name_cyr']    = name[0]
@@ -40,7 +41,7 @@ if __name__ == '__main__':
                                                      ('Елизавета', 'Elizaveta'),
                                                      ('Ксения', 'Ksenia'),
                                                      ('София', 'Sofia')],
-                                     surnames      =[(('Иванов', 'Ivanov'), ('Иванова', 'Ivanova')),
+                                       surnames    =[(('Иванов', 'Ivanov'), ('Иванова', 'Ivanova')),
                                                      (('Петров', 'Petrov'), ('Петрова', 'Petrova')),
                                                      (('Сидоров', 'Sidorov'), ('Сидорова', 'Sidorova')),
                                                      (('Смирнов', 'Smirnov'), ('Смирнова', 'Smirnova')),
@@ -49,8 +50,8 @@ if __name__ == '__main__':
                                                      (('Васильев', 'Vasiliev'), ('Васильева', 'Vasilieva')),
                                                      (('Макаров', 'Makarov'), ('Макарова', 'Makarova')),
                                                      (('Михайлов', 'Mikhailov'), ('Михайлова', 'Mikhailova'))],
-                                     group = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118],
-                                     mark1 = [2, 3, 4, 5],
-                                     mark2 = [2, 3, 4, 5],
-                                     mark3 = [2, 3, 4, 5])
+                                       group = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118],
+                                       mark1 = [2, 3, 4, 5],
+                                       mark2 = [2, 3, 4, 5],
+                                       mark3 = [2, 3, 4, 5])
     print(df.head())
